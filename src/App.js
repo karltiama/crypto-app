e409080ddd334eb8432de8ec413e43a49e1b06ff
Karl Tiama <karltiama@gmail.com>
2022-01-20 10:04:41 -0500

    add API functionality

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'     // add useState
 import  Axios from 'axios';
+import Coin from './components/Coin'      
 
 function App() {
+  const [listOfCoins, setListofCoins] = useState([]);
 
   useEffect(() => {
-    Axios.get("https://api.coinstats.app/public/v1/coins?skip=0&limit=10'").then(
+    Axios.get("https://api.coinstats.app/public/v1/coins?skip=0&limit=10").then(
       (response) => {
-          console.log(response.data);
+          // console.log(response.data);
+          setListofCoins(response.data.coins);
         }
       );
   }, []);
@@ -16,9 +19,19 @@ function App() {
   return (
     <div className="App">
       <div className="cryptoHeader"></div>
-      <div className="cryptopDisplay"></div>
+      <div className="cryptopDisplay">
+        {listOfCoins.map((coin) => {
+          return <Coin 
+              name={coin.name} 
+              icon={coin.icon} 
+              price={coin.price} 
+              symbol={coin.symbol} 
+            />
+        })}
+      </div>
     </div>
   );
 }
 
 export default App;
+

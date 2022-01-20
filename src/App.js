ff998fcb48a9b068149dc5a6bc8b014f0329b7f7
Karl Tiama <karltiama@gmail.com>
2022-01-20 09:11:24 -0500

    fetch data with useEffect

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import './App.css';
+import { useEffect } from 'react'
+import  Axios from 'axios';
 
 function App() {
+
+  useEffect(() => {
+    Axios.get("https://api.coinstats.app/public/v1/coins?skip=0&limit=10'").then(
+      (response) => {
+          console.log(response.data);
+        }
+      );
+  }, []);
+
+
   return (
     <div className="App">
       <div className="cryptoHeader"></div>
